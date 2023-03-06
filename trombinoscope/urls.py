1c8d8435e1fe35360b51055006541d01edce4ac4
rducrot <ducrot.romain@gmail.com>
2023-03-06 14:24:56 +0100

    Gestion des informations de l'utilisateur

--- trombinoscope/urls.py
+++ trombinoscope/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
 from django.views.generic.base import RedirectView
 
 import app.views
+import authentication.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +30,11 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='authentication/login.html',
                                redirect_authenticated_user=True, ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change.html'), name='password_change'),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'), name='password_change_done'),
+    path('account/', authentication.views.account, name='account'),
     path('home/', app.views.home, name='home'),
     path('service/<int:service_id>/', app.views.service, name='service'),
     path('annuaire', app.views.phone_book, name='annuaire'),
