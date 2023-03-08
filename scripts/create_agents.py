7f61a572acfb1756968bc120619dda07c62918c7
rducrot <ducrot.romain@gmail.com>
2023-03-08 14:24:21 +0100

    Script import services et agents

---
+++ scripts/create_agents.py
@@ -0,0 +1,41 @@
+import csv
+
+from app.models import Service, Agent
+
+
+def check_db():
+    if Agent.objects.exists():
+        answer = input("Attention ! Une base de données existe déjà. Souhaitez-vous la remplacer ? [o/N]")
+        if answer not in ['o', 'O', 'oui', 'Oui', 'y', 'Y']:
+            exit()
+
+
+def create_agents():
+    with open('scripts/agents.csv') as file:
+        reader = csv.DictReader(file, delimiter=";")
+
+        Agent.objects.all().delete()
+
+        for row in reader:
+            print(row)
+
+            if row["role"]:
+                agent = Agent(last_name=row["last_name"],
+                              first_name=row["first_name"],
+                              grade=row["grade"],
+                              registration_number=row["registration_number"],
+                              service=Service.objects.get(pk=int(row["service_pk"])),
+                              role=row["role"])
+            else:
+                agent = Agent(last_name=row["last_name"],
+                              first_name=row["first_name"],
+                              grade=row["grade"],
+                              registration_number=row["registration_number"],
+                              service=Service.objects.get(pk=int(row["service_pk"])))
+
+            agent.save()
+
+
+def run():
+    check_db()
+    create_agents()
---
+++ scripts/create_services.py
@@ -0,0 +1,35 @@
+import csv
+
+from app.models import Service
+
+
+def check_db():
+    if Service.objects.exists():
+        answer = input("Attention ! Une base de données existe déjà. Souhaitez-vous la remplacer ? [o/N]")
+        if answer not in ['o', 'O', 'oui', 'Oui', 'y', 'Y']:
+            exit()
+
+
+def create_services():
+    with open('scripts/services.csv') as file:
+        reader = csv.DictReader(file, delimiter=";")
+
+        Service.objects.all().delete()
+
+        for row in reader:
+            print(row)
+
+            if row["parent_pk"]:
+                service = Service(pk=int(row["pk"]),
+                                  name=row["name"],
+                                  parent=Service.objects.get(pk=int(row["parent_pk"])))
+            else:
+                service = Service(pk=int(row["pk"]),
+                                  name=row["name"])
+
+            service.save()
+
+
+def run():
+    check_db()
+    create_services()
